1b372076f563525de83f197219f7de664591b46c
icebabbyy <myndearlost@gmail.com>
2025-06-26 23:32:36 +0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -271,7 +271,6 @@ const Index = () => {
                   ? 'bg-green-100 text-green-800 border-green-300' 
                 : product.product_status === 'พรีออเดอร์'
                   ? 'bg-purple-100 text-purple-800 border-purple-300'
-                  : 'bg-gray-100 text-gray-800 border-gray-200'
               }`}
             >
             </Badge>
