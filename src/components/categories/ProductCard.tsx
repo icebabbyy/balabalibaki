8462dfe2e03b08ce99cc571e88c641c1973e6b38
icebabbyy <myndearlost@gmail.com>
2025-06-27 15:45:46 +0700

    Update ProductCard.tsx

--- src/components/categories/ProductCard.tsx
+++ src/components/categories/ProductCard.tsx
@@ -109,7 +109,7 @@ const ProductCard = ({ product }: { product: ProductPublic }) => {
                 className="cursor-pointer hover:bg-amber-100 border-amber-300 text-amber-800"
                 onClick={(e) => {
                   e.stopPropagation(); // ป้องกันไม่ให้ Card ทั้งใบถูกคลิกไปด้วย
-                  navigate(`/tag/${encodeURIComponent(tag)}`);
+                  navigate(`/products/tag/${tag}`);
                 }}
               >
                 #{tag}
