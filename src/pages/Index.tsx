54ed3988ee9e59a9121804c2df4264424cbb96da
icebabbyy <myndearlost@gmail.com>
2025-06-29 00:59:32 +0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,3 @@
-<head>
-  <meta charset="UTF-8" />
-  <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>WishyouLuckyShop</title>
-
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://tfwmvpvxilosiyewqjtk.supabase.co">
-
-</head>
 
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
