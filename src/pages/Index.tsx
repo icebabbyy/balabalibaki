bbf18a19d6b492468776dae8532563b9f4009f82
icebabbyy <myndearlost@gmail.com>
2025-06-29 00:53:36 +0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,14 @@
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Wishyoulucky's Shop</title>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://tfwmvpvxilosiyewqjtk.supabase.co">
+
+</head>
+
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { supabase } from "@/integrations/supabase/client";
